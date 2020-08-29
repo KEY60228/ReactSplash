afdd353479dacac8347594e63625984a67335b0a
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-08-29 15:30:17 +0900

    add Footer

--- src/front/src/components/App.tsx
+++ src/front/src/components/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import PhotoList from './PhotoList'
 import Login from './Login'
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" exact children={<PhotoList />}/>
           <Route path="/login" children={<Login />}/>
         </Switch>
+        <Footer />
       </BrowserRouter>
     </>
   )
