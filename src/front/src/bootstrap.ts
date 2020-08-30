07910725b63f95800fb47185e48a314c8947ba65
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-08-30 16:45:56 +0900

    install CSRF check

---
+++ src/front/src/bootstrap.ts
@@ -0,0 +1,14 @@
+import { getCookieValue } from './util'
+
+const window = {
+  axios: require('axios')
+}
+
+// Ajaxリクエストであることを示すヘッダーを付与する
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+window.axios.interceptors.request.use((config: any) => {
+  // クッキーからトークンを取り出してヘッダーに添付する
+  config.headers['X-XSRF-TOKEN'] = getCookieValue('XSRF-TOKEN')
+  return config
+})
