6a47c95b718778b47e495b7b09b385d46594613f
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-09-01 19:23:09 +0900

    modify cors settings (CORS Request is clear but maybe CSRF TOKEN has problems)

--- src/api/config/cors.php
+++ src/api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => env('CORS_ALLOWED_ORIGINS'),
+    'allowed_origins' => ['http://localhost:8081'],
 
     'allowed_origins_patterns' => [],
 
