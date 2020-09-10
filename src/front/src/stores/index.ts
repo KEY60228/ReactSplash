44402aac760ac30cb701c17fb9fac10821de7634
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-09-10 13:00:53 +0900

    install message slice

--- src/front/src/stores/index.ts
+++ src/front/src/stores/index.ts
@@ -4,10 +4,12 @@ import { configureStore } from '@reduxjs/toolkit'
 // それぞれ slice.reducer を default export している前提
 import authReducer from './auth'
 import errorReducer from './error'
+import messageReducer from './message'
 
 const reducer = combineReducers({
   auth: authReducer,
-  error: errorReducer
+  error: errorReducer,
+  message: messageReducer,
 })
 
 const store = configureStore({ reducer })
