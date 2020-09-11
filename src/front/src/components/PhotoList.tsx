8def6b39eff3e185cffdc2d4d6cc73567c4b7573
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-09-11 23:54:34 +0900

    modify PhotoList.tsx

--- src/front/src/components/PhotoList.tsx
+++ src/front/src/components/PhotoList.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
+import { useDispatch } from 'react-redux'
+
+import Photo from './Photo'
+import { setCode } from '../stores/error'
+import { OK } from '../util'
 
 const PhotoList = () => {
+  const dispatch = useDispatch()
+  const [photos, setPhotos]: [any, any] = useState([]);
+
+  const fetchPhotos = async() => {
+    const response = await window.axios.get('https://localhost:1443/api/photos')
+
+    if (response.status !== OK) {
+      dispatch(setCode(response.status))
+      return false
+    }
+
+    setPhotos(response.data.data)
+  }
+
+  useEffect(() => {
+    fetchPhotos()
+  }, [])
+
   return (
     <>
-      <h1>Photo List</h1>
+      <div className="photo-list">
+        <div className="grid">
+          { photos.map((photo: any, index: any) => {
+            return (<Photo key={index} item={photo} />)
+          })}
+        </div>
+      </div>
     </>
   )
 }
